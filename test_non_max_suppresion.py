48b05cf0ea8a6f04397505a50bff97861a7f0df2
Neil Rhodes <neil@pobox.com>
2018-10-28 10:22:04 -0700

    Updated changes to make random testing better

--- test_non_max_suppresion.py
+++ test_non_max_suppresion.py
@@ -29,6 +29,12 @@ class TestNonMaxSuppression(unittest.TestCase):
 
 
     def test_random_overlapping(self):
+        """Tests by creating a random set of boxes: one true one per class
+        and a number of random ones.
+
+        Don't start using this test until you've got all other,
+        more basic tests working."""
+
         def random_in_range(a_min, a_max):
             return a_min + random.random() * (a_max - a_min)
 
@@ -38,39 +44,40 @@ class TestNonMaxSuppression(unittest.TestCase):
         MIN_COORD = 0
         MAX_COORD = 1000
 
-        boxes = []
-        scores = []
-        classes = []
-        good_indexes = []
-        for c in range(0, 99):
-            # For each class, create one real box, and a number
-            # of boxes with lower scores that overlap the real box
-            left = random.randrange(MAX_COORD)
-            top = random.randrange(MAX_COORD)
-            right = random.randrange(left, MAX_COORD)
-            bottom = random.randrange(top, MAX_COORD)
-            real_box = [left, top, right, bottom]
-            real_score = random_in_range(.7, .9)
-            boxes.append(real_box)
-            scores.append(real_score)
-            classes.append(c)
-            if real_score > 0.4:
-                good_indexes.append(len(boxes) - 1)
-            for fake_boxes in range(0, 20):
-                h_factor = random_in_range(-.2, .2)
-                v_factor = random_in_range(-.2, .2)
-                delta_h = (right - left) * h_factor
-                delta_v = (bottom - top) * v_factor
-                fake_box = [
-                        left + delta_h, top + delta_v,
-                        right + delta_h, bottom + delta_v]
-                boxes.append(fake_box)
-                fake_score = random_in_range(real_score - .2, real_score - .001)
-                scores.append(fake_score)
+        for run in range(20):
+            boxes = []
+            scores = []
+            classes = []
+            good_indexes = []
+            for c in range(99):
+                # For each class, create one real box, and a number
+                # of boxes with lower scores that overlap the real box
+                left = random.randrange(MAX_COORD-1)
+                top = random.randrange(MAX_COORD-1)
+                right = random.randrange(left+1, MAX_COORD)
+                bottom = random.randrange(top+1, MAX_COORD)
+                real_box = [left, top, right, bottom]
+                real_score = random_in_range(.3, .9)
+                boxes.append(real_box)
+                scores.append(real_score)
                 classes.append(c)
+                if real_score > 0.4:
+                    good_indexes.append(len(boxes) - 1)
+                for fake_boxes in range(20):
+                    h_factor = random_in_range(-.2, .2)
+                    v_factor = random_in_range(-.2, .2)
+                    delta_h = (right - left) * h_factor
+                    delta_v = (bottom - top) * v_factor
+                    fake_box = [
+                            left + delta_h, top + delta_v,
+                            right + delta_h, bottom + delta_v]
+                    boxes.append(fake_box)
+                    fake_score = random_in_range(real_score - .2, real_score - .001)
+                    scores.append(fake_score)
+                    classes.append(c)
 
-        self.assertEqualAnyOrder(np.array(good_indexes),
-                nms(np.array(boxes), np.array(scores), np.array(classes)))
+                self.assertEqualAnyOrder(np.array(good_indexes),
+                    nms(np.array(boxes), np.array(scores), np.array(classes)))
 
 if __name__ == '__main__':
     unittest.main()
